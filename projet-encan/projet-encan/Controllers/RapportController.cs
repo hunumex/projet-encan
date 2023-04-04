7c8dcc2faaf102321b10e0d12ea1f11dfc242012
Taharqoo <128047802+Taharqoo@users.noreply.github.com>
2023-04-04 09:22:42 -0300

    la connection de la base de données

---
+++ projet-encan/projet-encan/Controllers/RapportController.cs
@@ -0,0 +1,20 @@
+﻿using Encan_Services.Services.S_Bidding;
+using Encan_Services.Services.S_Item;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace projet_encan.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RapportController : ControllerBase
+    {
+        readonly IBiddingService _biddingService;
+        readonly IItemService _itemService;
+        public RapportController(IBiddingService biddingService, IItemService itemService)
+        {
+            _biddingService = biddingService;
+            _itemService = itemService;
+        }
+    }
+}
